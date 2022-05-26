b4e6306b571e2779ce62c9aa9ba5a9edbba8bc00
Ezechiel ITIMBIEN KAKANA <papaezechiel@gmail.com>
2022-05-26 03:18:33 -0400

    create server end client

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express  = require("express");
+const socketio = require("socket.io");
+
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+
+// reel communication
+const io = socketio(server);
+
+app.get('./', (req, res) => {
+  res.json({result: 'ok'});
+})
+
+io.on('connection', (socket) => {
+  console.log("Someone has connected");
+})
+
+
+server.listen(8080, () => console.log('app is listen in port 8080'));
