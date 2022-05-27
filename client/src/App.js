da3bade3e2f9972608b4a8cc53055fef3b89dfc2
Ezechiel ITIMBIEN KAKANA <papaezechiel@gmail.com>
2022-05-27 01:00:07 -0400

    client

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,35 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 // to connect our client with server, we need
 // to install socket.io-client
 // first in dependencies end connect here
 import socketIoClient from "socket.io-client";
 
 const ENDPOINT = "http://localhost:8080";
+const connection = socketIoClient(ENDPOINT);
 
 function App() {
+  const [user, setUser] = useState('');
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    connection.on('INITIAL', (data) => {
+      console.log("DATA");
+      console.log(data);
+      setUser(data.name);
+      setUsers(data.users);
+    })
+
+    connection.on('NEW_USER', data => {
+      console.log(data);
+      setUsers(data.users);
+    })
+
+  }, [])
+
   return (
     <div className="App">
-      <p>Hello world App.js</p>
+     {user ? <h1>logged in as: {user}</h1> : <h1>Loading...</h1>}
+     {users.map(user => <li>{user}</li>)}
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -4,17 +4,24 @@ const socketio = require("socket.io");
 const http = require('http');
 const app = express();
 const server = http.createServer(app);
+const ikea = require('ikea-name-generator');
 
 // reel communication
 const io = socketio(server);
-
 app.get('/', (req, res) => {
   res.json({result: 'ok'});
+
+})
+const users = [];
+
+io.on('connection', (socket) => {
+  const name = ikea.getName();
+  users.push(name);
+  socket.emit('INITIAL', {name, users});
+  socket.emit('NEW_USER', {name});
+  console.log("Someone has connected", name);
 })
 
-io.on('connection', () => {
-  console.log("Someone has connected");
-})
 
 
 server.listen(8080, () => console.log('app is listen in port 8080'));
